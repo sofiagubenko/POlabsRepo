980a27e879183353c5f377141c8cd17e875bbcf1
sofiagubenko <sofisofa170@gmail.com>
2025-04-07 20:43:03 +0300

    final version

--- po_lab2/lab2.cpp
+++ po_lab2/lab2.cpp
@@ -14,13 +14,13 @@ using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
 
 mutex mtx;
-long long ParEvenSum = 0;
-int ParMinEven = -1;
+long long evenSum = 0;
+int minEven = -1;
 
 atomic<long long> AtomicEvenSum(0);
 atomic<int> AtomicMinEven;
 
-void processArrayPart(const vector<int>& arr, int start, int end) 
+void processArrayPart(const vector<int>& arr, int start, int end, bool useMutex) 
 {
     long long localEvenSum = 0;
     int localMinEven = -1;
@@ -37,13 +37,20 @@ void processArrayPart(const vector<int>& arr, int start, int end)
         }
     }
 
-    lock_guard<mutex> lock(mtx);
-    ParEvenSum += localEvenSum;
-    if (localMinEven != -1) 
+    if (useMutex)
     {
-        if (ParMinEven == -1 || localMinEven < ParMinEven) 
+        lock_guard<mutex> lock(mtx);
+        evenSum += localEvenSum;
+        if (localMinEven != -1 && (minEven == -1 || localMinEven < minEven)) 
         {
-            ParMinEven = localMinEven;
+                minEven = localMinEven;
+        }
+    } else 
+    {
+        evenSum += localEvenSum;
+        if (localMinEven != -1 && (minEven == -1 || localMinEven < minEven)) 
+        {
+                minEven = localMinEven;
         }
     }
 }
@@ -74,142 +81,134 @@ void processArrayPartAtomic(const vector<int>& arr, int start, int end)
 
 int main()
 {
-    int size;
-    cout << "Enter the size of the array: ";
-    cin >> size;
+    vector<int> sizes = {100000, 1000000, 10000000, 100000000, 1000000000, 2000000000};
+    vector<int> threadsList = {2, 4, 8, 16, 32, 64, 128};
 
-    if(size <= 0)
+    for (int i = 0; i < sizes.size(); i++) 
     {
-        cout << "Invalid array size" << endl;
-        return 0;
-    }
+        int size = sizes[i];
 
-    vector<int> arr(size);
-    srand(static_cast<unsigned>(time(nullptr)));
+        cout << "\n===============================\n";
+        cout << "Array size: " << size << endl;
+        
+        vector<int> arr(size);
+        srand(static_cast<unsigned>(time(nullptr)));
 
-    for(int i = 0; i < size; i++)
-    {
-        arr[i] = rand() % 10000;
-    }
-    
-    cout << "\n----- SEQUENTIAL VERSION -----\n";
-
-    long long evenSum = 0;
-    int minEven = -1;
-
-    auto seq_begin = high_resolution_clock::now();
-
-    for (int i = 0; i < size; i++) 
-    {
-        if (arr[i] % 2 == 0) 
+        for(int j = 0; j < size; j++)
         {
-            evenSum += arr[i];
-            if (minEven == -1 || arr[i] < minEven) 
-            {
-                minEven = arr[i];
-            }
+            arr[j] = rand() % 10000;
         }
-    }
-    auto seq_end = high_resolution_clock::now();
-    auto seq_time = duration_cast<microseconds>(seq_end - seq_begin);
+        
+        cout << "\nSEQUENTIAL VERSION\n";
 
-    //
-    cout << "Sum of even numbers: " << evenSum << endl;
-    if (minEven != -1) 
-    {
+        evenSum = 0;
+        minEven = -1;
+
+        auto seq_begin = high_resolution_clock::now();
+
+        processArrayPart(arr, 0, size, false);
+    
+        auto seq_end = high_resolution_clock::now();
+        auto seq_time = duration_cast<microseconds>(seq_end - seq_begin);
+        double seconds = seq_time.count() / 1000000.0;
+
+        //
+        cout << "Sum of even numbers: " << evenSum << endl;
         cout << "Smallest even number: " << minEven << endl;
-    } 
-    cout << "Time taken: " << seq_time.count() << " microseconds" << endl;
-    //
+        cout << "Time taken: " << seq_time.count() << " microseconds" << endl;
+        cout << "Time taken: " << seconds << " seconds" << endl;
+        //
 
 
-    const int numThreads = 128;
-    int chunkSize = size / numThreads;
+        cout << "\nPARALLEL VERSION(blocking primitives)\n";
 
-    cout << "\n----- PARALLEL VERSION(blocking primitives) -----\n";
-
-    ParEvenSum = 0;
-    ParMinEven = -1;
-
-    vector<thread> threads;
-
-    auto parallel_begin = high_resolution_clock::now();
-
-    for (int i = 0; i < numThreads; i++)
-    {
-        int start = i * chunkSize;
-        int end;
-        if (i == numThreads - 1) 
+        for (int t = 0; t < threadsList.size(); t++)
         {
-            end = size;
-        } else 
-        {
-            end = (i + 1) * chunkSize;
+            int numThreads = threadsList[t];
+            evenSum = 0;
+            minEven = -1;
+
+            vector<thread> threads;
+            int chunkSize = size / numThreads;
+
+            auto parallel_begin = high_resolution_clock::now();
+
+            for (int k = 0; k < numThreads; k++)
+            {
+                int start = k * chunkSize;
+                int end;
+                if (k == numThreads - 1) 
+                {
+                    end = size;
+                } else 
+                {
+                    end = (k + 1) * chunkSize;
+                }
+                threads.push_back(thread(processArrayPart, ref(arr), start, end, true));
+            }
+            
+            for (auto& g : threads) 
+            {
+                if (g.joinable()) 
+                {
+                    g.join();
+                } 
+            }
+            auto parallel_end = high_resolution_clock::now();
+            auto parallel_time = duration_cast<microseconds>(parallel_end - parallel_begin);
+            double seconds = parallel_time.count() / 1000000.0;
+
+            // 
+            cout << numThreads << " threads - time " << parallel_time.count() << " microseconds\n";
+            cout << "Time taken: " << seconds << " seconds" << endl;
+            //
         }
-        threads.push_back(thread(processArrayPart, ref(arr), start, end));
-    }
-    
-    for (auto& t : threads) 
-    {
-        if (t.joinable()) 
+        
+        cout << "\nPARALLEL VERSION(atomic CAS)\n";
+
+        for (int t = 0; t < threadsList.size(); t++)
         {
-            t.join();
-        } 
-    }
-    auto parallel_end = high_resolution_clock::now();
-    auto parallel_time = duration_cast<microseconds>(parallel_end - parallel_begin);
+            int numThreads = threadsList[t];
+            AtomicEvenSum.store(0);
+            AtomicMinEven.store(-1);
 
-    //
-    cout << "Sum of even numbers: " << ParEvenSum << endl;
-    if (ParMinEven != -1) 
-    {
-        cout << "Smallest even number: " << ParMinEven << endl;
-    } 
-    cout << "Time taken: " << parallel_time.count() << " microseconds" << endl;
-    //
-    
-    
-    cout << "\n----- PARALLEL VERSION(atomic CAS) -----\n";
+            vector<thread> atomicThreads;
+            int chunkSize = size / numThreads;
 
-    AtomicEvenSum.store(0);
-    AtomicMinEven.store(-1);
-    vector<thread> atomicThreads;
+            auto parallel_atomic_begin = high_resolution_clock::now();
 
-    auto parallel_atomic_begin = high_resolution_clock::now();
+            for (int k = 0; k < numThreads; k++) 
+            {
+                int start = k * chunkSize;
+                int end;
+            
+                if (k == numThreads - 1) 
+                {
+                    end = size;
+                } else 
+                {
+                    end = (k + 1) * chunkSize;
+                }
 
-    for (int i = 0; i < numThreads; i++) 
-    {
-        int start = i * chunkSize;
-        int end;
-    
-        if (i == numThreads - 1) 
-        {
-            end = size;
-        } else 
-        {
-            end = (i + 1) * chunkSize;
-        }
+                atomicThreads.push_back(thread(processArrayPartAtomic, ref(arr), start, end));
+            }    
 
-        atomicThreads.push_back(thread(processArrayPartAtomic, ref(arr), start, end));
-    }    
+            for (auto& t : atomicThreads) 
+            {
+                if (t.joinable()) 
+                {
+                    t.join();
+                }
+            }
 
-    for (auto& t : atomicThreads) 
-    {
-        if (t.joinable()) 
-        {
-            t.join();
+            auto parallel_atomic_end = high_resolution_clock::now();
+            auto parallel_atomic_time = duration_cast<microseconds>(parallel_atomic_end - parallel_atomic_begin);
+            double seconds = parallel_atomic_time.count() / 1000000.0;
+            
+            cout << numThreads << " threads - time " << parallel_atomic_time.count() <<  " microseconds\n";
+            cout << "Time taken: " << seconds << " seconds" << endl;
         }
     }
-
-    auto parallel_atomic_end = high_resolution_clock::now();
-    auto parallel_atomic_time = duration_cast<microseconds>(parallel_atomic_end - parallel_atomic_begin);
-    
-    cout << "Sum of even numbers: " << AtomicEvenSum.load() << endl;
-    if (AtomicMinEven.load() != -1)
-    {
-        cout << "Smallest even number: " << AtomicMinEven.load() << endl;
-    }
-    cout << "Time taken: " << parallel_atomic_time.count() << " microseconds" << endl;
 
     return 0;
 }
