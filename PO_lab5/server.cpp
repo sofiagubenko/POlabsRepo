57e027460034136814ef72034eb22f7258cb4668
sofiagubenko <sofisofa170@gmail.com>
2025-04-24 20:59:42 +0300

    start

---
+++ PO_lab5/server.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib, "ws2_32.lib")
+#define PORT 8080
+
+int main()
+{
+
+
+    return 0;
+}
